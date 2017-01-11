792246ded64b89d3476b786dde294f65b12676a3
martindobrev <martin@dobrev.eu.com>
2017-01-11 19:05:19 +0200

    maven project initialized

---
+++ src/main/java/com/maddob/App.java
@@ -0,0 +1,13 @@
+package com.maddob;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
