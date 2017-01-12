116972a814449633c368265e108a48dc282defe9
martindobrev <martin@dobrev.eu.com>
2017-01-12 17:13:28 +0200

    Thymeleaf template engine added

--- src/main/java/com/maddob/server/MadWebServerVerticle.java
+++ src/main/java/com/maddob/server/MadWebServerVerticle.java
@@ -4,6 +4,7 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.templ.ThymeleafTemplateEngine;
 
 /**
  * WebServer verticle for the blog
@@ -24,6 +25,22 @@ public class MadWebServerVerticle extends AbstractVerticle {
         super.start();
         server = getVertx().createHttpServer();
         Router router = Router.router(getVertx());
+
+
+        // In order to use a Thymeleaf template we first need to create an engine
+        final ThymeleafTemplateEngine engine = ThymeleafTemplateEngine.create();
+        router.get("/template").handler(ctx -> {
+            ctx.put("welcome", "Hi mad dogs! This is a message from thymeleaf!");
+
+            engine.render(ctx, "templates/index.html", res -> {
+                if (res.succeeded()) {
+                    ctx.response().end(res.result());
+                } else {
+                    ctx.fail(res.cause());
+                }
+            });
+        });
+
         router.route("/*").handler(StaticHandler.create());
         server.requestHandler(router::accept).listen(8080);
     }
